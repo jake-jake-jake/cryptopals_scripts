bb700d9f9df9032283d4e7a855ff3e7e53078710
Jake Davis <jcdavis@awedge.net>
2016-01-12 22:12:08 +0000

    Add func to confirm ECB and detect blocklength
    Added a confirm_ECB function that inserts incrementally
    more bytes to determine if ECB is being used. If it finds
    a duplicate block, it returns one half the increment inserted
    as int.

--- challenge12.py
+++ challenge12.py
@@ -1,7 +1,8 @@
+#!/usr/bin/env python3
+
 # Challenge 12
 
 import binascii
-from collections import Counter
 from Crypto.Cipher import AES
 import os
 
@@ -9,7 +10,7 @@ def prepend_bytes(target_bytes, insertion_byte = b'A', insertion_multiple = 32):
     ''' Prepends a number of bytes before target data; defaults to 32.'''
     insertion = insertion_byte * insertion_multiple
     return insertion + target_bytes
-  
+
 def PKCS7_pad(data, block_length):
     ''' Pad data with PKCS7 padding.'''
     padding = block_length - (len(data) % block_length)
@@ -28,22 +29,27 @@ def detect_AES_ECB_mode(ciphertext, blocksize = 16):
     else:
         return False
 
-insert_byte = b'A'
+def confirm_ECB(target, key):
+    ''' Return likely blocklength of ECB mode cipher.'''
+    for _ in range(1, 33):
+        prepended_target = prepend_bytes(target, insertion_multiple = _)
+        encrypted_target = ECB_encrypt(prepended_target, key)
+        if detect_AES_ECB_mode(encrypted_target):
+            print('Duplicate blocks of data with {} byte insertion, suggesting {} byte block length.'.format(_, _//2))
+            return _//2
+        else:
+            continue
+    print('Unable to confirm ECB mode.')
+    return False
+
 b64_data = '''Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkg
                  aGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBq
                  dXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUg
                  YnkK'''
+b64_bytes = binascii.a2b_base64(b64_data)
+static_key = os.urandom(16)
 
-key = os.urandom(16)
+def crack_target(target, key):
+    pass
 
-
-def break_ECB_target(target, key):
-    b64_bytes = binascii.a2b_base64(target)
-    prepended_target = prepend_bytes(target_bytes=b64_bytes)
-    encrypted_target = ECB_encrypt(prepended_target, key)
-    print(encrypted_target)
-
-print(break_ECB_target(b64_data, key=key))
-
-
-
+print(confirm_ECB(b64_bytes, static_key))
