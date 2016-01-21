2a380dff98947304c8b2bc3298db2ec7028642a8
Jake <jcdavis@awedge.net>
2016-01-21 14:19:07 -0500

    Add dummy email variables and length reporting
    Added dummy email variables and length reporting.

--- challenge13.py
+++ challenge13.py
@@ -9,9 +9,6 @@ import os
 def parse_cookie(s):
     return s.replace('&', ';')
 
-def make_amp(s):
-    return s.replace(';', '&')
-
 def profile_for(email='foo@bar.com', id_num=10, role='user'):
     if '&' in email or '=' in email:
         raise ValueError('No "&" or "=" chars allowed in email.')
@@ -27,15 +24,21 @@ def ECB_encrypt(data, key):
     ECB_cipher = AES.new(key, AES.MODE_ECB)
     return ECB_cipher.encrypt(PKCS7_pad(data, 16))
 
-key = os.urandom(16)
+def ECB_decrypt(cipher, key):
+    ''' Decript ECB cipher using provided key'''
+    ECB_cipher = AES.new(key, AES.MODE_ECB)
+    return ECB_cipher.decrypt(PKCS7_pad(cipher, 16))
 
+key = os.urandom(16)
 c = cookies.BaseCookie()
 
 dummy_morsels = 'foo=bar&baz=qux&zap=zazzle'
+dummy_email = 'aaaa@bbbbb.com'
+dummy_profile = profile_for(dummy_email)
+encrypted_dummy = ECB_encrypt(bytes(dummy_profile, encoding='utf-8'), key=key)
+
+print('Dummy email length: {}'.format(len(dummy_email)))
+print('Dummy profile length: {}'.format(len(dummy_profile)))
+print('Dummy encryped cookie length: {}'.format(len(encrypted_dummy)))
 
 
-
-
-
-
-print(profile_for('jakejakejake@gmail.com'))
