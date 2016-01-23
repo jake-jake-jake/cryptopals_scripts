fffd6fc523e296d8ea302f313fdc1d94fcab4556
jake-jake-jake <jcdavis@awedge.net>
2016-01-22 23:57:39 -0500

    Fixed error in ECB decrypt function
    I'd copied and pasted the encrypt function without removing the call to pad
    the input; this was creating stray cruft. It's now removed, and the cookie is
    properly formed. Just need to write a PKCS stripping function and then feed
    the finished cookie into the parser.

--- challenge13.py
+++ challenge13.py
@@ -3,8 +3,6 @@
 from Crypto.Cipher import AES
 import os
 
-
-
 def parse_cookie(s):
     ''' Return dictionary from structured literal.'''
     return dict(x.split(b'=') for x in s.split(b'&'))
@@ -28,7 +26,7 @@ def ECB_encrypt(data, key):
 def ECB_decrypt(cipher, key):
     ''' Decrypt ECB cipher using provided key'''
     ECB_cipher = AES.new(key, AES.MODE_ECB)
-    return ECB_cipher.decrypt(PKCS7_pad(cipher, 16))
+    return ECB_cipher.decrypt(cipher)
 
 def cookie_oracle(email, key):
     ''' Return an ECB encrypted cookie from an email.'''
@@ -41,7 +39,7 @@ static_key = os.urandom(16)
 # These are crafted to do the work. If our profile_for function stripped
 # padding characters this method would not work.
 insertion_block = PKCS7_pad(b'admin', 16)
-print(insertion_block, len(insertion_block))
+print("insertion_block", insertion_block, len(insertion_block))
 bait_email = b'xx@aol.com'
 switch_email = b'xxxxxx@aol.com'
 
@@ -51,9 +49,9 @@ print(profile_for(bait_email + insertion_block))
 # padding to mimic a final block of a cipher. Then create a prefix that should
 # terminate just in time to accept our crafted 'admin' block.
 attack_suffix = cookie_oracle((bait_email+insertion_block), static_key)[16:32]
-print(ECB_decrypt(attack_suffix,static_key))
+print("attack_suffix", ECB_decrypt(attack_suffix,static_key), len(attack_suffix))
 attack_prefix = cookie_oracle((switch_email), static_key)[:32]
-print(ECB_decrypt(attack_prefix, static_key))
+print("attack_prefix", ECB_decrypt(attack_prefix, static_key))
 
 # Put pieces together.
 finished_cookie = ECB_decrypt(attack_prefix + attack_suffix, static_key)
