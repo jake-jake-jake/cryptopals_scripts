44460b98a8ecb5ab97c31f6cbf751897354f2b03
Jake <jcdavis@awedge.net>
2016-01-29 14:27:49 -0500

    Clean up challenge 15 code
    I cleaned up the code on Challenge 15; I believe it will catch most instances
    of invalid padding now, but it is possible that some random bytes could read
    as valid padding through it. Without adding a block_length param, I don't
    know if that can be avoided.

--- challenge15.py
+++ challenge15.py
@@ -8,16 +8,15 @@ def PKCS7_pad(data, block_length):
     return (data + bytes([padding])*padding)
 
 def PKCS7_unpad(data):
-    # if data[-1] > len(data)//2:
-    #     raise ValueError('Invalid padding.')
+    ''' Remove PKCS7 padding.'''
     if data[-1] == 0 or not len(set(data[-data[-1]:])) == 1:
         raise ValueError('Invalid padding.')
-    # elif not len(set(data[-data[-1]:])) == 1:
-    #     raise ValueError('Invalid padding.')
     return data[:len(data)-data[-1]]
 
 admin_invalid = b'yellowsubmarine\x02'
 admin_valid = PKCS7_pad(b'admin', 16)
+valid_sub = b'yellow submarin\x01'
 
 print(PKCS7_unpad(admin_valid))
-admin_unpadded = PKCS7_unpad(admin_invalid)
+print(PKCS7_unpad(valid_sub))
+print(PKCS7_unpad(admin_invalid))
