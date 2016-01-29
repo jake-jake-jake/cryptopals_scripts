d73438ab6edee9a736b04256ec4c1bfba9b96f4d
Jake <jcdavis@awedge.net>
2016-01-29 14:10:42 -0500

    Add find_admin function to check for admin in data

--- challenge16.py
+++ challenge16.py
@@ -10,21 +10,31 @@ def PKCS7_pad(data, block_length):
     padding = block_length - (len(data) % block_length)
     return (data + bytes([padding])*padding)
 
-def CBC_oracle(byte_string):
+def CBC_encrypt_oracle(byte_string):
     prefix = b'comment1=cooking%20MCs;userdata='
     suffix = b';comment2=%20like%20a%20pound%20of%20bacon'
     cipher = AES.new(static_key, AES.MODE_CBC, static_IV)
 
     # clean the input string
     if b';' in byte_string or b'=' in byte_string:
-        byte_string = byte_string.replace(b';', b'%3B')
-        byte_string = byte_string.replace(b'=', b'%3D')
-    print('Debug:', prefix + byte_string + suffix)
+        byte_string = byte_string.replace(b';', b'";"')
+        byte_string = byte_string.replace(b'=', b'"="')
     return cipher.encrypt(PKCS7_pad(prefix + byte_string + suffix, 16))
+
+def find_admin(byte_string):
+    cipher = AES.new(static_key, AES.MODE_CBC, static_IV)
+    decrypted = cipher.decrypt(ciphertext=byte_string)
+    if b';admin=true;' in decrypted:
+        return True 
+    else:
+        return False
+
+
 
 test = b'this=this=admin;hahahaha'
 static_key = os.urandom(16)
 static_IV = os.urandom(16)
 
 
-print(CBC_oracle(test))
+print(CBC_encrypt_oracle(test))
+print(find_admin(CBC_encrypt_oracle(test)))
