f589a99d05e46491c41269a34046542b82e12200
Jake <jcdavis@awedge.net>
2016-02-04 16:35:24 -0500

    Clean up debug code
    Removed the debug code.

--- challenge18.py
+++ challenge18.py
@@ -25,11 +25,5 @@ nonce = struct.pack('<Q', 0)
 encrypted = 'L77na/nrFsKvynd6HzOoG7GHTLXsTVu9qvY/2syLXzhPweyyMTJULu/6/kXX0KSvoOLSFQ=='
 encrypted_bytes = binascii.a2b_base64(encrypted)
 
-a_string = b'This is a thing that I want to encrypt.'
+print('encrypted_bytes:', decrypt_AES_CTR(static_key, nonce, encrypted_bytes))
 
-print('nonce:', nonce)
-print(decrypt_AES_CTR(static_key, nonce, encrypted_bytes))
-
-encrypted_test = encrypt_AES_CTR(static_key, nonce, a_string)
-print('encrypted_test:', encrypted_test)
-print('decrypted_dest:', decrypt_AES_CTR(static_key, nonce, encrypted_test))
