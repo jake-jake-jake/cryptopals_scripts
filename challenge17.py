5810f8f967c1952f8e7d4d0d061d007d37d39428
Jake <jcdavis@awedge.net>
2016-02-08 17:59:29 -0500

    Broke find_valid_padding_bytes, added xor functions
    I broke the first function trying to make it recursively add bytes. I'll need
    to fix it, as well as break up the "work_block" param into an IV and target
    block.

--- challenge17.py
+++ challenge17.py
@@ -42,16 +42,46 @@ def make_work_blocks(ciphertext, IV):
     print('DEBUG in make_work_blocks:\nlength of ciphtertext, blocks', len(ciphertext), len(concatenated))
     return [concatenated[i:i+32] for i in range(0, len(concatenated)-16, 16)]
 
-def find_valid_padding_bytes(work_block, padding_oracle):
-    ''' Iterate through all final last bytes of first block to find valid padding.'''
-    print('DEBUG in find_valid_padding_bytes:\nlength of work_block', len(work_block))
-    prefix = work_block[:15]
-    possibles = [prefix + bytes([i]) for i in range(256)]
-    print('DEBUG in find_valid_padding_bytes:\n length of possibles', len(possibles))
-    for possible in possibles:
-        if padding_oracle(work_block[16:], possible):
-            print(possible[15])
+def validate_single_padding_block(cipherblock, IV, padding_oracle):
     pass
+
+def update_known_padding(pad):
+    ''' Return bytes should produce one more valid byte of PKCS7 padding when
+        tried against values for next byte in IV. '''
+    return [bytes([a ^ b] for a,b in zip(pad, bytes([len(pad)]) * len(pad)))]
+
+
+def find_valid_padding_bytes(work_block, padding_oracle, pad_bytes = []):
+    ''' Iterate through IV bytes to find valid padding, beginning with last byte.'''
+    print('DEBUG in find_valid_padding_bytes:\nlength of work_block', len(work_block))
+    # Prefix length is one less than the length of pad_bytes. On first
+    # run, this will be an empty list and so working on the last byte.
+    prefix = work_block[:16-len(pad_bytes)-1]
+    new_known_bytes = update_known_padding(pad_bytes)
+    possible_IVs = [prefix + bytes([i]) + b''.join(reversed(new_known_bytes)) for i in range(256)]
+    print('DEBUG in find_valid_padding_bytes:\n length of possibles', len(possible_IVs))
+    for IV in possible_IVs:
+        if padding_oracle(work_block[16:], IV):
+            print(IV[:16-len(pad_bytes)-1])
+            new_known_bytes.append(IV[:16-len(pad_bytes)-1])
+            break
+    else:
+        print('DEBUG: Unable to find valid padding byte')
+        return prefix + b''.join(reversed(new_known_bytes))
+
+    # if len(work_list) > 1:
+    #     print('DEBUG: Multiple possible valid padding bytes; must pass the choices to a function to find the valid \\x01 byte.')
+    # elif len(work_list) == 1:
+    #     pad_bytes.append(work_list[0])
+    # else: 
+    #     print('DEBUG: Unable to find valid padding byte.')
+    if len(new_known_bytes) < 16:
+        new_work_block = prefix + b''.join(reversed(new_known_bytes)) + work_block[16:]
+        return find_valid_padding_bytes(new_work_block, padding_oracle, new_known_bytes)
+
+    if len(passed_bytes) == 16:
+        return b''.join(reversed(passed_bytes)) 
+
 
 def decrypt_block(work_block, padding_oracle):
     ''' Working from padding_oracle output, decrypt second block of CBC block pair.'''
@@ -60,7 +90,7 @@ def decrypt_block(work_block, padding_oracle):
 def attack_CBC_via_padding_oracle(ciphertext, instance_IV):
     ''' Using instance_IV and padding oracle, decrypt ciphertext.'''
     work_blocks = make_work_blocks(ciphertext, instance_IV)
-    print(work_blocks)
+    # print(work_blocks)
     for block in work_blocks:
         print('Trying block now:\n {}'.format(block))
         find_valid_padding_bytes(block, check_padding_CBC)
@@ -70,5 +100,3 @@ static_key = os.urandom(16)
 encrypted, this_IV = random_string_CBC(static_key)
 
 attack_CBC_via_padding_oracle(encrypted, this_IV)
-
-
