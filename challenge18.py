b4376adf949beadb7c26b7a1d791e183242d1025
Jake <jcdavis@awedge.net>
2016-02-15 12:07:39 -0500

    Fiddle with Challenge18.py

--- challenge18.py
+++ challenge18.py
@@ -25,4 +25,11 @@ nonce = struct.pack('<Q', 0)
 encrypted = 'L77na/nrFsKvynd6HzOoG7GHTLXsTVu9qvY/2syLXzhPweyyMTJULu/6/kXX0KSvoOLSFQ=='
 encrypted_bytes = binascii.a2b_base64(encrypted)
 
+test_literals = [b'A' * 48, b'B' * 48, b'C' * 48]
+
+for literal in test_literals:
+    encrypt_literal = encrypt_AES_CTR(static_key, nonce, literal)
+    for i in range(0,len(encrypt_literal),16):
+        print(encrypt_literal[i:i+16])
+
 print('encrypted_bytes:', decrypt_AES_CTR(static_key, nonce, encrypted_bytes))
