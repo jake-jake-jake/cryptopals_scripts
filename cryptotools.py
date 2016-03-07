6f1c4755808d470335a442de86aba0a7e611c14d
Jake <jcdavis@awedge.net>
2016-03-07 11:58:14 -0500

    Removed typo character from bytes_xor() function

--- cryptotools.py
+++ cryptotools.py
@@ -17,7 +17,7 @@ def bytes_xor(a, b):
     ''' Xor two bytestrings of equal length.'''
     if len(a) != len(b):
         raise ValueError('Xored bytestrings must be of equal length.')
-    return bytes([x ^ y for x,y in zip(a, b)])ß
+    return bytes([x ^ y for x,y in zip(a, b)])
 
 
 def bytes_to_base64(b):
